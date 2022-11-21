0bb8b1308b5c9ecb5efb445e5ab6d33fc476d3d5
Niek <Frankenstein795@gmail.com>
2022-11-21 11:03:37 +0100

    Update OpenAPI.json

--- app/main.py
+++ app/main.py
@@ -44,7 +44,7 @@ def custom_openapi():
 
     openapi_schema = get_openapi(
         title="F1 API",
-        version="1.2.3",
+        version="1.2.5",
         description="An easier way to use the [ergast.com](https://ergast.com/mrd/) F1 API, with correct types!",
         license_info={
             "name": "MIT",
